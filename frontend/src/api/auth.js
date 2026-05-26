805eddf5a51c4de02fd5c113de1eec9b27dc645b
panbomba2230 <burgjan2230@gmail.com>
2026-05-26 18:56:32 +0200

    feat: add manual outfit creation, favorites for clothes and outfits, cleanup dead auth helpers

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,28 +1,3 @@
-import api from './axiosConfig'; 
-
-export const login = async (username, password) => {
-  const response = await api.post('auth/login/', {
-    login: username, 
-    password: password,
-  });
-  return response.data;
-};
-
-export const register = async (username, password) => {
-  const response = await api.post('auth/register/', {
-    username: username,
-    password: password,
-  });
-  return response.data;
-};
-
-export const resetPassword = async (email) => {
-  const response = await api.post('auth/reset-password/', {
-    email: email,
-  });
-  return response.data;
-};
-
 export const isAuthenticated = () => {
   return !!localStorage.getItem('access_token');
 };
